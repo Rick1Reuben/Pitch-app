b4da29dfd590dd0669fbb7e17580392408126616
Ryan Anzaye <rileydollar8@gmail.com>
2024-05-08 00:27:18 +0300

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import './App.css'; 
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="main">
+        <div className="navbar">
+          <div className="icon">
+            <h2 className="logo">Pitch</h2>
+          </div>
+          <div className="menu">
+            <ul>
+            <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/bookpitch">BookPitch</a>
+              </li>
+              <li>
+                <a href="/review">Reviews</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="tent">
+          <h1>Welcome to Pitch</h1>
+          <p className="par">Where we find your best football pitch to play in “Score your perfect play: Where every kick finds its pitch!"</p>
+          <div className="search">
+            <input className="srch" type="search" name="" placeholder="Search" />
+          </div>
+          <div className="container">
+            <footer>
+              <p>Pitch</p>
+              <p>
+                For more information contact us at info@pitch.co.ke or follow us on our social media platforms at pitch.ke
+              </p>
+              <div className="social">
+                <a href="#">
+                  <i className="fab fa-instagram"></i>
+                </a>
+                <a href="#">
+                  <i className="fab fa-linkedin"></i>
+                </a>
+                <a href="#">
+                  <i className="fab fa-whatsapp"></i>
+                </a>
+              </div>
+              <div className="copy">
+                <p>&copy; 2024 Pitch. All Rights Reserved</p>
+              </div>
+            </footer>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     
+    
     </>
   )
 }
 
-export default App
+export default App;
